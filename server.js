5be00003629d698d53271506c175def48ae8b15b
Kent <myai.kentteng@gmail.com>
2025-11-29 13:39:44 +0800

    fix: correct axios import statement in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
